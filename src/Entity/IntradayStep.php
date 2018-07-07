4d9a0bbff4a9d47fb0b1b16b09b2ba9263e15033
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2018-07-07 12:24:23 +0100

    feat(objects): Added full IntradayStep support

---
+++ src/Entity/IntradayStep.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\IntradayStepRepository")
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="DateReading", columns={"patient_id","date","hour"})})
+ *
+ * @ApiResource
+ * @ApiFilter(SearchFilter::class, properties={"id": "exact", "date": "exact", "patient": "exact", "hour": "exact"})
+ */
+class IntradayStep
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="integer", length=2, nullable=true)
+     */
+    private $hour;
+
+    /**
+     * @ORM\Column(type="integer", length=6, nullable=true)
+     */
+    private $value;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Patient")
+     * @ORM\JoinColumn(name="patient_id", referencedColumnName="id")
+     */
+    private $patient;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHour(): ?int
+    {
+        return $this->hour;
+    }
+
+    public function setHour(?int $hour): self
+    {
+        $this->hour = $hour;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(?int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getPatient(): ?Patient
+    {
+        return $this->patient;
+    }
+
+    public function setPatient(?Patient $patient): self
+    {
+        $this->patient = $patient;
+
+        return $this;
+    }
+}
