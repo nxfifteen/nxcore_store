f487b4d04e8c64939d7f33405cbd745bdfc03a97
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2018-07-07 12:46:25 +0100

    feat(objects): Added friends object

---
+++ src/Entity/Friends.php
@@ -0,0 +1,149 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FriendseRepository")
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="FriendId", columns={"patient_id","service","remote_id"})})
+ *
+ * @ApiResource
+ * @ApiFilter(SearchFilter::class, properties={"id": "exact", "remote_id": "exact", "patient": "exact", "gender": "exact", "thirdPartyService": "exact"})
+ */
+class Friends
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $remote_id;
+
+    /**
+     * @ORM\Column(type="string", length=200, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $avatar;
+
+    /**
+     * @ORM\Column(type="string", length=6, nullable=true)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="integer", length=3, nullable=true)
+     */
+    private $rank;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Patient")
+     * @ORM\JoinColumn(name="patient_id", referencedColumnName="id")
+     */
+    private $patient;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\ThirdPartyService")
+     * @ORM\JoinColumn(name="service", referencedColumnName="id")
+     */
+    private $thirdPartyService;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRemoteId(): ?string
+    {
+        return $this->remote_id;
+    }
+
+    public function setRemoteId(?string $remote_id): self
+    {
+        $this->remote_id = $remote_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(?string $avatar): self
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getRank(): ?int
+    {
+        return $this->rank;
+    }
+
+    public function setRank(?int $rank): self
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+
+    public function getPatient(): ?Patient
+    {
+        return $this->patient;
+    }
+
+    public function setPatient(?Patient $patient): self
+    {
+        $this->patient = $patient;
+
+        return $this;
+    }
+
+    public function getThirdPartyService(): ?ThirdPartyService
+    {
+        return $this->thirdPartyService;
+    }
+
+    public function setThirdPartyService(?ThirdPartyService $thirdPartyService): self
+    {
+        $this->thirdPartyService = $thirdPartyService;
+
+        return $this;
+    }
+}
---
+++ src/Repository/FriendseRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Friends;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Friends|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Friends|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Friends[]    findAll()
+ * @method Friends[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FriendseRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Friends::class);
+    }
+
+//    /**
+//     * @return Friends[] Returns an array of Friends objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('f.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Friends
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
