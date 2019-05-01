782f41da63b5b4c707b9de6e347b682b200029c5
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-05-01 19:34:01 +0100

    docs(DocBlock): Added project boiler plate to all files

--- src/Repository/ThirdPartyRelationsRepository.php
+++ src/Repository/ThirdPartyRelationsRepository.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+* This file is part of the Storage module in NxFIFTEEN Core.
+*
+* Copyright (c) 2019. Stuart McCulloch Anderson
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @package     Store
+* @version     0.0.0.x
+* @since       0.0.0.1
+* @author      Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+* @link        https://nxfifteen.me.uk NxFIFTEEN
+* @link        https://git.nxfifteen.rocks/nx-health NxFIFTEEN Core
+* @link        https://git.nxfifteen.rocks/nx-health/store NxFIFTEEN Core Storage
+* @copyright   2019 Stuart McCulloch Anderson
+* @license     https://license.nxfifteen.rocks/mit/2015-2019/ MIT
+*/
+
 namespace App\Repository;
 
 use App\Entity\ThirdPartyRelations;
