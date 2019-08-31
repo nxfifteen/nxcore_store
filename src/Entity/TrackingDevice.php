a69bb54f7ac761b8e1c78e5aff18cf863db75306
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-08-31 18:15:31 +0100

    refactor(v2): Made $manufacturer nullable

--- src/Entity/TrackingDevice.php
+++ src/Entity/TrackingDevice.php
@@ -51,7 +51,7 @@ class TrackingDevice
     private $type;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $manufacturer;
 
