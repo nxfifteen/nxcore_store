709d52c7fb678052f64b24898aaf739743187261
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-08-31 14:25:17 +0100

    feat(v2): Changed ID to auto increment

--- src/Entity/TrackingDevice.php
+++ src/Entity/TrackingDevice.php
@@ -15,7 +15,7 @@ class TrackingDevice
 {
     /**
      * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
      */
     private $id;
