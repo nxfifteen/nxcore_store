3e52c84f3766a565cb5b2923ada6c1577b167d93
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-08-31 13:49:03 +0100

    feat(v2): Added stub security class

---
+++ src/Security/TokenAuthenticator.php
@@ -0,0 +1,122 @@
+<?php
+// src/Security/TokenAuthenticator.php
+namespace App\Security;
+
+use App\Entity\Patient;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // check credentials - e.g. make sure the password is valid
+        // no credential check is needed in this case
+
+        // return true to cause authentication success
+        return TRUE;
+    }
+
+    /**
+     * Called on every request. Return whatever credentials you want to
+     * be passed to getUser() as $credentials.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function getCredentials(Request $request)
+    {
+        return [
+            'token' => $request->headers->get('X-AUTH-TOKEN'),
+        ];
+    }
+
+    /**
+     * @param mixed                 $credentials
+     * @param UserProviderInterface $userProvider
+     *
+     * @return Patient|null|object|UserInterface
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiToken = $credentials['token'];
+
+        if (NULL === $apiToken) {
+            return null;
+        }
+
+        // if a User object, checkCredentials() is called
+        return $this->em->getRepository(Patient::class)
+            ->findOneBy(['apiToken' => $apiToken]);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // on success, let the request continue
+        return NULL;
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     *
+     * @param Request                      $request
+     * @param AuthenticationException|null $authException
+     *
+     * @return JsonResponse
+     */
+    public function start(Request $request, AuthenticationException $authException = NULL)
+    {
+        $data = [
+            // you might translate this message
+            'message' => 'Authentication Required',
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Called on every request to decide if this authenticator should be
+     * used for the request. Returning false will cause this authenticator
+     * to be skipped.
+     *
+     * @param Request $request
+     *
+     * @return bool
+     */
+    public function supports(Request $request)
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    public function supportsRememberMe()
+    {
+        return FALSE;
+    }
+}
