ebec926267d86d550ddc0b3601898f10d9433e80
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-08-31 18:19:45 +0100

    refactor(v2): Created an App constant and a log write method

---
+++ src/AppConstants.php
@@ -0,0 +1,37 @@
+<?php
+namespace App;
+
+
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+class AppConstants
+{
+    const SAMSUNGHEALTH = "samasung";
+    const SAMSUNGHEALTHSERVICE = "Samsung Health";
+
+    static function writeToLog(String $fileName, String $body) {
+        try {
+            $path = sys_get_temp_dir() . '/sync_upload_post';
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+        }
+
+        if (!empty($path)) {
+            $file = $path . '/' . $fileName;
+
+            $fileSystem = new Filesystem();
+            try {
+                $fileSystem->mkdir($path);
+                if ($fileSystem->exists($file)) {
+                    $fileSystem->appendToFile($file, $body . "\n");
+                } else {
+                    $fileSystem->dumpFile($file, $body . "\n");
+                }
+
+            } catch (IOExceptionInterface $exception) {
+                echo "An error occurred while creating your directory at " . $exception->getPath();
+            }
+        }
+    }
+}
