de6303e9b6c0bfc964602d0118fd272b85b87065
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-08-31 23:23:29 +0100

    refactor(v2): installed symfony/monolog-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
