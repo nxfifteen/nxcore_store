0e3cdfdbce1da36e4e9261ad36d0df02f4a0e81c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-09-01 10:51:47 +0100

    feat(v2): DB Rollup

--- src/Migrations/Version20190901092912.php
+++ src/Migrations/Version20190901092912.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190901092708 extends AbstractMigration
+final class Version20190901092912 extends AbstractMigration
 {
     public function getDescription() : string
     {
