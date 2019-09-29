d81c327b1bf20dbc78e0f8224736987a9ceb3dbc
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2019-09-29 15:59:00 +0100

    feat(feed): Added XP to the json feed

--- src/Controller/FeedJsonController.php
+++ src/Controller/FeedJsonController.php
@@ -16,6 +16,18 @@ class FeedJsonController extends AbstractController
     /** @var Patient $patient */
     private $patient;
 
+    /**
+     * @Route("/json/count/daily/steps", name="json_daily_step")
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function FitStepsDailySummary()
+    {
+        $this->setupRoute();
+
+        return $this->FitStepsDailySummaryDateTracker(date("Y-m-d"), -1);
+    }
+
     private function setupRoute()
     {
         if (is_null($this->patient)) $this->patient = $this->getUser();
@@ -30,35 +42,11 @@ class FeedJsonController extends AbstractController
     }
 
     /**
-     * @Route("/json/count/daily/steps", name="json_daily_step")
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function FitStepsDailySummary( )
-    {
-        $this->setupRoute();
-
-        return $this->FitStepsDailySummaryDateTracker(date("Y-m-d"), -1);
-    }
-
-    /**
-     * @Route("/json/count/daily/steps/{trackingDevice}", name="json_daily_step_trackingDevice")
-     *
-     * @param int $trackingDevice
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function FitStepsDailySummaryTracker(int $trackingDevice)
-    {
-        $this->setupRoute();
-
-        return $this->FitStepsDailySummaryDateTracker(date("Y-m-d"), $trackingDevice);
-    }
-
-    /**
      * @Route("/json//count/daily/steps/{trackingDevice}/{date}", name="json_daily_step_date_trackingDevice")
      *
      * @param String $date
-     * @param int $trackingDevice
+     * @param int    $trackingDevice
+     *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function FitStepsDailySummaryDateTracker(String $date, int $trackingDevice)
@@ -71,36 +59,36 @@ class FeedJsonController extends AbstractController
             ->findByDateRange($this->patient->getUuid(), $date, $trackingDevice);
 
         $timeStampsInTrack = [];
-        $timeStampsInTrack[ 'uuid' ] = $this->patient->getUuid();
-        $timeStampsInTrack[ 'today' ] = $date;
-        $timeStampsInTrack[ 'lastReading' ] = $date;
-        $timeStampsInTrack[ 'sum' ] = 0;
-        $timeStampsInTrack[ 'goal' ] = 0;
-        $timeStampsInTrack[ 'values' ] = [];
+        $timeStampsInTrack['uuid'] = $this->patient->getUuid();
+        $timeStampsInTrack['today'] = $date;
+        $timeStampsInTrack['lastReading'] = $date;
+        $timeStampsInTrack['sum'] = 0;
+        $timeStampsInTrack['goal'] = 0;
+        $timeStampsInTrack['values'] = [];
 
-        if ( count($product) > 0 ) {
+        if (count($product) > 0) {
             $goals = 0;
 
             /** @var FitStepsDailySummary[] $product */
-            foreach ( $product as $item ) {
-                if ( is_numeric($item->getValue()) ) {
-                    $timeStampsInTrack[ 'sum' ] = $timeStampsInTrack[ 'sum' ] + $item->getValue();
+            foreach ($product as $item) {
+                if (is_numeric($item->getValue())) {
+                    $timeStampsInTrack['sum'] = $timeStampsInTrack['sum'] + $item->getValue();
                     if (is_numeric($item->getGoal()->getGoal())) {
                         $goals = $goals + 1;
                         $timeStampsInTrack['goal'] = $item->getGoal()->getGoal();
                     }
 
                     $recordItem = [];
-                    $recordItem[ 'dateTime' ] = $item->getDateTime()->format("H:i:s");
-                    $recordItem[ 'value' ] = $item->getValue();
-                    if (!is_null($item->getTrackingDevice())) $recordItem[ 'tracker' ] = $item->getTrackingDevice()->getName();
-                    if (!is_null($item->getTrackingDevice()->getService())) $recordItem[ 'service' ] = $item->getTrackingDevice()->getService()->getName();
+                    $recordItem['dateTime'] = $item->getDateTime()->format("H:i:s");
+                    $recordItem['value'] = $item->getValue();
+                    if (!is_null($item->getTrackingDevice())) $recordItem['tracker'] = $item->getTrackingDevice()->getName();
+                    if (!is_null($item->getTrackingDevice()->getService())) $recordItem['service'] = $item->getTrackingDevice()->getService()->getName();
 
-                    $timeStampsInTrack[ 'values' ][] = $recordItem;
+                    $timeStampsInTrack['values'][] = $recordItem;
                 }
             }
-            if ( isset($item) ) {
-                $timeStampsInTrack[ 'lastReading' ] = $item->getDateTime()->format("H:i:s");
+            if (isset($item)) {
+                $timeStampsInTrack['lastReading'] = $item->getDateTime()->format("H:i:s");
             }
 
             if ($goals > 0) {
@@ -114,11 +102,25 @@ class FeedJsonController extends AbstractController
     }
 
     /**
+     * @Route("/json/count/daily/steps/{trackingDevice}", name="json_daily_step_trackingDevice")
+     *
+     * @param int $trackingDevice
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function FitStepsDailySummaryTracker(int $trackingDevice)
+    {
+        $this->setupRoute();
+
+        return $this->FitStepsDailySummaryDateTracker(date("Y-m-d"), $trackingDevice);
+    }
+
+    /**
      * @Route("/json/count/daily/water", name="json_daily_water")
      *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function consumeWater( )
+    public function consumeWater()
     {
         $this->setupRoute();
 
@@ -129,9 +131,10 @@ class FeedJsonController extends AbstractController
      * @Route("/json/count/daily/water/{date}", name="json_daily_water_date")
      *
      * @param String $date
+     *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function consumeWaterDate(String $date )
+    public function consumeWaterDate(String $date)
     {
         $this->setupRoute();
 
@@ -141,31 +144,31 @@ class FeedJsonController extends AbstractController
             ->findByDateRange($this->patient->getUuid(), $date);
 
         $timeStampsInTrack = [];
-        $timeStampsInTrack[ 'uuid' ] = $this->patient->getUuid();
-        $timeStampsInTrack[ 'today' ] = $date;
-        $timeStampsInTrack[ 'lastReading' ] = "00:00:00";
-        $timeStampsInTrack[ 'sum' ] = 0;
-        $timeStampsInTrack[ 'goal' ] = 0;
-        $timeStampsInTrack[ 'values' ] = [];
+        $timeStampsInTrack['uuid'] = $this->patient->getUuid();
+        $timeStampsInTrack['today'] = $date;
+        $timeStampsInTrack['lastReading'] = "00:00:00";
+        $timeStampsInTrack['sum'] = 0;
+        $timeStampsInTrack['goal'] = 0;
+        $timeStampsInTrack['values'] = [];
 
-        if ( count($product) > 0 ) {
+        if (count($product) > 0) {
             /** @var ConsumeWater[] $product */
-            foreach ( $product as $item ) {
-                if ( is_numeric($item->getMeasurement()) ) {
-                    $timeStampsInTrack[ 'sum' ] = $timeStampsInTrack[ 'sum' ] + $item->getMeasurement();
-                    if ($timeStampsInTrack[ 'goal' ] == 0) $timeStampsInTrack[ 'goal' ] = $item->getPatientGoal()->getGoal();
+            foreach ($product as $item) {
+                if (is_numeric($item->getMeasurement())) {
+                    $timeStampsInTrack['sum'] = $timeStampsInTrack['sum'] + $item->getMeasurement();
+                    if ($timeStampsInTrack['goal'] == 0) $timeStampsInTrack['goal'] = $item->getPatientGoal()->getGoal();
 
                     $recordItem = [];
-                    $recordItem[ 'dateTime' ] = $item->getDateTime()->format("H:i:s");
-                    $recordItem[ 'value' ] = $item->getMeasurement();
-                    $recordItem[ 'comment' ] = $item->getComment();
-                    if (!is_null($item->getTrackingDevice())) $recordItem[ 'service' ] = $item->getTrackingDevice()->getName();
+                    $recordItem['dateTime'] = $item->getDateTime()->format("H:i:s");
+                    $recordItem['value'] = $item->getMeasurement();
+                    $recordItem['comment'] = $item->getComment();
+                    if (!is_null($item->getTrackingDevice())) $recordItem['service'] = $item->getTrackingDevice()->getName();
 
-                    $timeStampsInTrack[ 'values' ][] = $recordItem;
+                    $timeStampsInTrack['values'][] = $recordItem;
                 }
             }
-            if ( isset($item) ) {
-                $timeStampsInTrack[ 'lastReading' ] = $item->getDateTime()->format("H:i:s");
+            if (isset($item)) {
+                $timeStampsInTrack['lastReading'] = $item->getDateTime()->format("H:i:s");
             }
         }
 
@@ -177,7 +180,7 @@ class FeedJsonController extends AbstractController
      *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function body( )
+    public function body()
     {
         $this->setupRoute();
 
@@ -188,9 +191,10 @@ class FeedJsonController extends AbstractController
      * @Route("/json/count/daily/body/{date}", name="json_daily_body_date")
      *
      * @param String $date
+     *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function bodyDate(String $date )
+    public function bodyDate(String $date)
     {
         $this->setupRoute();
 
@@ -207,17 +211,37 @@ class FeedJsonController extends AbstractController
         $bodyFats = $bodyFats[0];
 
         $timeStampsInTrack = [];
-        $timeStampsInTrack[ 'uuid' ] = $this->patient->getUuid();
-        $timeStampsInTrack[ 'today' ] = $date;
-        $timeStampsInTrack[ 'lastReading' ] = $bodyWeights->getDateTime()->format("H:i:s");
-        $timeStampsInTrack[ 'value_kg' ] = $bodyWeights->getMeasurement();
-        $timeStampsInTrack[ 'goal_kg' ] = $bodyWeights->getPatientGoal()->getGoal();
-        $timeStampsInTrack[ 'value_lb' ] = round($bodyWeights->getMeasurement() * 2.205, 2);
-        $timeStampsInTrack[ 'goal_lb' ] = round($bodyWeights->getPatientGoal()->getGoal() * 2.205, 2);
-        $timeStampsInTrack[ 'fat' ] = round($bodyFats->getMeasurement(), 2);
-        $timeStampsInTrack[ 'goal_fat' ] = round($bodyFats->getPatientGoal()->getGoal(), 2);
+        $timeStampsInTrack['uuid'] = $this->patient->getUuid();
+        $timeStampsInTrack['today'] = $date;
+        $timeStampsInTrack['lastReading'] = $bodyWeights->getDateTime()->format("H:i:s");
+        $timeStampsInTrack['value_kg'] = $bodyWeights->getMeasurement();
+        $timeStampsInTrack['goal_kg'] = $bodyWeights->getPatientGoal()->getGoal();
+        $timeStampsInTrack['value_lb'] = round($bodyWeights->getMeasurement() * 2.205, 2);
+        $timeStampsInTrack['goal_lb'] = round($bodyWeights->getPatientGoal()->getGoal() * 2.205, 2);
+        $timeStampsInTrack['fat'] = round($bodyFats->getMeasurement(), 2);
+        $timeStampsInTrack['goal_fat'] = round($bodyFats->getPatientGoal()->getGoal(), 2);
 
         return $this->json($timeStampsInTrack);
     }
 
+    /**
+     * @Route("/json/rpg/xp", name="json_rpg_xp")
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function rpgXp()
+    {
+        $this->setupRoute();
+
+        $returnArray = [];
+        $returnArray['uuid'] = $this->patient->getUuid();
+        $returnArray['today'] = date("Y-m-d");
+        $returnArray['lastReading'] = date("H:i:s");
+        $returnArray['current'] = round($this->patient->getXpTotal(), 2);
+        $returnArray['next'] = ceil($returnArray['current'] / 100) * 100;
+        $returnArray['level'] = round($this->patient->getRpgLevel(), 2);
+        $returnArray['log'] = $this->patient->getXp()->last()->getReason();
+
+        return $this->json($returnArray);
+    }
 }
