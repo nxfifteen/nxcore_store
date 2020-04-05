c3eb914df8674ec23fdf652ba42f1564fede8138
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-05 22:28:11 +0100

    chore(comment): Updated copyright year in header block
    Issue #45

--- src/Command/DownloadHistoryFitbit.php
+++ src/Command/DownloadHistoryFitbit.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of NxFIFTEEN Fitness Core.
  *
- * Copyright (c) 2019. Stuart McCulloch Anderson
+ * Copyright (c) 2020. Stuart McCulloch Anderson
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
