db2e085700108ffd28c70b6e51ecf242483a133a
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-06 12:44:09 +0100

    chore(installable): Ignored unresolved include in kernel
    Issue #49

--- src/Kernel.php
+++ src/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends BaseKernel
     {
         date_default_timezone_set($_ENV['TIMEZONE']);
 
+        /** @noinspection PhpIncludeInspection */
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
