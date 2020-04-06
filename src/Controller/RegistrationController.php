7041d9cb526ec4b3109ad3f96982bae8cbe20655
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-06 12:06:29 +0100

    chore(installable): Ignore DuplicatedCode errors
    Issue #49

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,6 +9,7 @@
  * @copyright Copyright (c) 2020. Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
  * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
  */
+/** @noinspection DuplicatedCode */
 
 namespace App\Controller;
 
