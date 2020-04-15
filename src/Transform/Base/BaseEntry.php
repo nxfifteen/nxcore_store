8944a929a349ee1536ab1953935813fc99eef7e6
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-15 20:41:24 +0100

    feat(transform): Added feature to post to mirror

--- src/Transform/Base/BaseEntry.php
+++ src/Transform/Base/BaseEntry.php
@@ -29,8 +29,46 @@ class BaseEntry
      */
     protected function postToMirror($request)
     {
-        if (method_exists($request, "toJson")) {
-            AppConstants::writeToLog('mirror.txt', __LINE__ . $request->toJson());
+        if (method_exists($request, "toJson") && array_key_exists("MESH_MIRROR",
+                $_ENV) && !empty($_ENV['MESH_MIRROR'])) {
+            $mirror_url = $_ENV['MESH_MIRROR'] . '/store/sync/upload/Core/' . str_ireplace("App\Entity\\", "",
+                    get_class($request));
+            $dataPacket = json_decode($request->toJson(), true);
+            $dataPacket['AccessToken'] = 'blabla';
+            $dataPacket['NodeToken'] = 'bleble';
+            $dataPacket = json_encode($dataPacket);
+
+            $ch = curl_init($mirror_url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $dataPacket);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            $response = curl_exec($ch);
+            if (!empty($response)) {
+                $response = json_decode($response);
+                if ($response->success) {
+                    AppConstants::writeToLog('mirror.txt',
+                        '[' . $_ENV['MESH_MIRROR'] . '] Accepted the post to ' . $request->getRequestUri());
+                } else {
+                    if ($response->status != 200) {
+                        AppConstants::writeToLog('mirror.txt',
+                            '[' . $_ENV['MESH_MIRROR'] . '] Failed to accept the post to ' . $request->getRequestUri());
+                        AppConstants::writeToLog('mirror.txt',
+                            '[' . $_ENV['MESH_MIRROR'] . '] responded with ' . json_encode($response));
+                    } else {
+                        AppConstants::writeToLog('mirror.txt',
+                            '[' . $_ENV['MESH_MIRROR'] . '] responded with ' . $response->status);
+                    }
+                }
+            } else {
+                AppConstants::writeToLog('mirror.txt',
+                    '[' . $_ENV['MESH_MIRROR'] . '] sent an empty responce');
+            }
+
+            curl_close($ch);
         }
     }
 
