4cb01d6dafc64e53f6ce1dc9dd37d8ca2a9db01e
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-15 20:24:28 +0100

    fix(transform): Created base classes for common methods

---
+++ src/Transform/Base/BaseIntraDaySteps.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of NxFIFTEEN Fitness Core.
+ *
+ * @link      https://nxfifteen.me.uk/projects/nx-health/store
+ * @link      https://nxfifteen.me.uk/projects/nx-health/
+ * @link      https://git.nxfifteen.rocks/nx-health/store
+ * @author    Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @copyright Copyright (c) 2020. Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
+ */
+
+namespace App\Transform\Base;
+
+
+use App\Transform\Transform;
+
+class BaseIntraDaySteps extends Transform
+{
+
+}
