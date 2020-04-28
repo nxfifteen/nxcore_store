32117e700185eeb7d70e113b29cb021b1c98de53
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-28 23:01:23 +0100

    fix(samsung): Removed fitbit service name

--- src/Transform/SamsungHealth/Constants.php
+++ src/Transform/SamsungHealth/Constants.php
@@ -2,9 +2,8 @@
 /**
  * This file is part of NxFIFTEEN Fitness Core.
  *
- * @link      https://nxfifteen.me.uk/projects/nx-health/store
- * @link      https://nxfifteen.me.uk/projects/nx-health/
- * @link      https://git.nxfifteen.rocks/nx-health/store
+ * @link      https://nxfifteen.me.uk/projects/nxcore/
+ * @link      https://gitlab.com/nx-core/store
  * @author    Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
  * @copyright Copyright (c) 2020. Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
  * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
@@ -24,10 +23,6 @@ use App\Transform\Transform;
  */
 class Constants extends Transform
 {
-    /**
-     *
-     */
-    const FITBITSERVICE = "Fitbit";
     /**
      *
      */
