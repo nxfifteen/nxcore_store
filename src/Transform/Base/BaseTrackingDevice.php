02d3e5e5e005e7ac031c6567c3aad0ca2f14138c
Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
2020-04-28 23:00:50 +0100

    feat(tranform): Created base device class

---
+++ src/Transform/Base/BaseTrackingDevice.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of NxFIFTEEN Fitness Core.
+ *
+ * @link      https://nxfifteen.me.uk/projects/nxcore/
+ * @link      https://gitlab.com/nx-core/store
+ * @author    Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @copyright Copyright (c) 2020. Stuart McCulloch Anderson <stuart@nxfifteen.me.uk>
+ * @license   https://nxfifteen.me.uk/api/license/mit/license.html MIT
+ */
+
+namespace App\Transform\Base;
+
+
+use App\Transform\Transform;
+
+class BaseTrackingDevice extends Transform
+{
+
+}
